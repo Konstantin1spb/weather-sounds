83911a45176ac43d07c87a8badb4795ef13aeb65
Konstantin1spb <105925642+sitepreview@users.noreply.github.com>
2024-11-12 13:31:48 +0300

    feat: switch to typescript

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
 import styled from 'styled-components';
 
-const AppContainer = ({className}) => {
+interface Props {
+	className?: string
+	target?: HTMLInputElement
+}
+
+const AppContainer = ({className}: Props) => {
 	const audioSummer = new Audio('./assets/sounds/summer.mp3')
 	const audioRain = new Audio('./assets/sounds/rain.mp3')
 	const audioWinter = new Audio('./assets/sounds/winter.mp3')
 
 	document.body.style.transition = "background-image 0.5s"
 
-	const onSummerAudioClick = () => {
+	const onSummerAudioClick = (): void => {
 		audioRain.load()
 		audioWinter.load()
 		if(audioSummer.paused) {
@@ -20,7 +24,7 @@ const AppContainer = ({className}) => {
 		}
 	}
 
-	const onRainAudioClick = () => {
+	const onRainAudioClick = (): void => {
 		audioSummer.load()
 		audioWinter.load()
 		if(audioRain.paused) {
@@ -32,7 +36,7 @@ const AppContainer = ({className}) => {
 		}
 	}
 
-	const onWinterAudioClick = () => {
+	const onWinterAudioClick = (): void => {
 		audioSummer.load()
 		audioRain.load()
 		if(audioWinter.paused) {
@@ -44,10 +48,12 @@ const AppContainer = ({className}) => {
 		}
 	}
 
-	const onVolumeChange = ({target}) => {
-		audioSummer.volume = target.value
-		audioRain.volume = target.value
-		audioWinter.volume = target.value
+	const onVolumeChange = ({target}: Props): void => {
+		if(target) {
+			audioSummer.volume = Number(target.value)
+			audioRain.volume = Number(target.value)
+			audioWinter.volume = Number(target.value)
+		}
 	}
 
 	return(
@@ -55,13 +61,13 @@ const AppContainer = ({className}) => {
 			<h1>Weather Sounds</h1>
 			<div className="weather-container">
 				<div className="weather summer" onClick={onSummerAudioClick}>
-					<svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
+					<svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
 				</div>
 				<div className="weather rain" onClick={onRainAudioClick}>
-					<svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-cloud-rain"><line x1="16" y1="13" x2="16" y2="21"></line><line x1="8" y1="13" x2="8" y2="21"></line><line x1="12" y1="15" x2="12" y2="23"></line><path d="M20 16.58A5 5 0 0 0 18 7h-1.26A8 8 0 1 0 4 15.25"></path></svg>
+					<svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-cloud-rain"><line x1="16" y1="13" x2="16" y2="21"></line><line x1="8" y1="13" x2="8" y2="21"></line><line x1="12" y1="15" x2="12" y2="23"></line><path d="M20 16.58A5 5 0 0 0 18 7h-1.26A8 8 0 1 0 4 15.25"></path></svg>
 				</div>
 				<div className="weather winter" onClick={onWinterAudioClick}>
-					<svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-cloud-snow"><path d="M20 17.58A5 5 0 0 0 18 8h-1.26A8 8 0 1 0 4 16.25"></path><line x1="8" y1="16" x2="8.01" y2="16"></line><line x1="8" y1="20" x2="8.01" y2="20"></line><line x1="12" y1="18" x2="12.01" y2="18"></line><line x1="12" y1="22" x2="12.01" y2="22"></line><line x1="16" y1="16" x2="16.01" y2="16"></line><line x1="16" y1="20" x2="16.01" y2="20"></line></svg>
+					<svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-cloud-snow"><path d="M20 17.58A5 5 0 0 0 18 8h-1.26A8 8 0 1 0 4 16.25"></path><line x1="8" y1="16" x2="8.01" y2="16"></line><line x1="8" y1="20" x2="8.01" y2="20"></line><line x1="12" y1="18" x2="12.01" y2="18"></line><line x1="12" y1="22" x2="12.01" y2="22"></line><line x1="16" y1="16" x2="16.01" y2="16"></line><line x1="16" y1="20" x2="16.01" y2="20"></line></svg>
 				</div>
 			</div>
 			<input type="range" min="0" max="1" step="0.1" onChange={onVolumeChange} />
@@ -128,6 +134,7 @@ export const App = styled(AppContainer)`
 		max-width: 100px;
 		height: 7px;
 		margin-top: 50px;
+		appearance: none;
 		-webkit-appearance: none;
 		border-radius: 5px;
 		cursor: pointer;
--- webpack.config.common.js
+++ webpack.config.common.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
-	entry: './index.jsx',
+	entry: './index.tsx',
 	output: {
 		filename: '[name].[contenthash].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
